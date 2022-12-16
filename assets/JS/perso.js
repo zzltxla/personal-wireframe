3ece0a5eb67720593bc92bf3c9b42df1179baed6
zzltxla <ata@edenschool.fr>
2022-12-16 09:03:13 +0100

    third commit

--- assets/JS/perso.js
+++ assets/JS/perso.js
@@ -7,3 +7,34 @@ let positionElement = (e)=> {
     myCursor.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
 }
 window.addEventListener('mousemove', positionElement);
+//cursor function region ends here
+
+let slides = document.querySelectorAll(".slide-img");
+let slideIndex = 1;
+let eventMoveX = client.X
+
+showSlides(slideIndex);
+function changeSlides(n) {
+    showSlides((slideIndex += n));
+}
+function currentSlide(n) {
+    showSlides((slideIndex = n));
+}
+function showSlides(n) {
+    let i;
+    let slides = document.getElementById("slide");
+    if (n > slides.length) {
+        slideIndex = 1;
+    } 
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    Array.from(slides).forEach(item => (item.style.display = "none"));
+    slides[slideIndex - 1].style;display = "block";
+}
+
+
+
+slides.addEventListener('afterbegin', `${eventMoveX}`, () => 
+    changeSlides(1) 
+);
